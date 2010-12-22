b91c68ce08478a58f69e4b9b5e2188f0dfef3d70
Matthew A. Russell <matthew@zaffra.com>
2010-12-22 17:10:10 -0600

    More refactoring to use local twitter__* utilities

--- python_code/friends_followers__friends_followers_in_common.py
+++ python_code/friends_followers__friends_followers_in_common.py
@@ -1,27 +1,45 @@
 # -*- coding: utf-8 -*-
 
+import sys
+import redis
 
-def pp(_int):  # for nice number formatting
-    return locale.format('%d', _int, True)
+from twitter__util import getRedisIdByScreenName
 
+# A pretty-print function for numbers
+from twitter__util import pp
+
+r = redis.Redis()
 
 def friendsFollowersInCommon(screen_names):
-    r.sinterstore('temp$friends_in_common', [getRedisIdByScreenName(screen_name,
-                  'friend_ids') for screen_name in screen_names])
+    r.sinterstore('temp$friends_in_common', 
+                  [getRedisIdByScreenName(screen_name, 'friend_ids') 
+                      for screen_name in screen_names]
+                 )
+
     r.sinterstore('temp$followers_in_common',
                   [getRedisIdByScreenName(screen_name, 'follower_ids')
-                  for screen_name in screen_names])
+                      for screen_name in screen_names]
+                 )
 
-    print '-' * 50
-    print 'Friends in common for %s: %s' % (','.join(screen_names),
+    print 'Friends in common for %s: %s' % (', '.join(screen_names),
             pp(r.scard('temp$friends_in_common')))
-    print 'Followers in common for %s: %s' % (','.join(screen_names),
-            pp(r.scard('temp$followers_in_common')))
-    print '-' * 50
 
-    # clean up scratch workspace
+    print 'Followers in common for %s: %s' % (', '.join(screen_names),
+            pp(r.scard('temp$followers_in_common')))
+
+    # Clean up scratch workspace
 
     r.delete('temp$friends_in_common')
     r.delete('temp$followers_in_common')
 
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print >> sys.stderr, "Please supply at least two screen names."
+        sys.exit(1)
 
+    # Note:
+    # The assumption is that the screen names you are 
+    # supplying have already been added to Redis.
+    # See friends_followers__get_friends__refactored.py
+
+    friendsFollowersInCommon(sys.argv[1:])
