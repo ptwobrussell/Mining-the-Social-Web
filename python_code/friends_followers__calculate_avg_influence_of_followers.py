1a61128aeb99b8f44eb6b64a43d2adaa35c5a47f
Matthew A. Russell <matthew@zaffra.com>
2010-12-22 17:41:18 -0600

    refactoring a listing that computes someone's most popular followers to use twitter__util

--- python_code/friends_followers__calculate_avg_influence_of_followers.py
+++ python_code/friends_followers__calculate_avg_influence_of_followers.py
@@ -5,29 +5,22 @@ import json
 import locale
 import redis
 
+# Pretty printing numbers
+from twitter__util import pp 
+
+# These functions create consistent keys from 
+# screen names and user id values
+from twitter__util import getRedisIdByScreenName 
+from twitter__util import getRedisIdByUserId
+
 SCREEN_NAME = sys.argv[1]
 
 locale.setlocale(locale.LC_ALL, '')
 
-
-def pp(_int):  # for nice number formatting
-    return locale.format('%d', _int, True)
-
-
-def getRedisIdByScreenName(screen_name, key_name):
-    return 'screen_name$' + screen_name + '$' + key_name
-
-
-def getRedisIdByUserId(user_id, key_name):
-    return 'user_id$' + str(user_id) + '$' + key_name
-
-
 def calculate():
-    r = redis.Redis()  # default connection settings on localhost
+    r = redis.Redis()  # Default connection settings on localhost
 
-    screen_name = SCREEN_NAME
-
-    follower_ids = list(r.smembers(getRedisIdByScreenName(screen_name,
+    follower_ids = list(r.smembers(getRedisIdByScreenName(SCREEN_NAME,
                         'follower_ids')))
 
     followers = r.mget([getRedisIdByUserId(follower_id, 'info.json')
@@ -44,7 +37,7 @@ def calculate():
 
     # It could take a few minutes to calculate freqs, so store a snapshot for later use
 
-    r.set(getRedisIdByScreenName(screen_name, 'follower_freqs'),
+    r.set(getRedisIdByScreenName(SCREEN_NAME, 'follower_freqs'),
           json.dumps(freqs))
 
     keys = freqs.keys()
@@ -58,9 +51,9 @@ def calculate():
 
     all_freqs = [k for k in keys for user in freqs[k]]
     avg = reduce(lambda x, y: x + y, all_freqs) / len(all_freqs)
-    print "The average number of followers for %s's followers: %s" \
-        % (screen_name, pp(avg))
 
+    print "\nThe average number of followers for %s's followers: %s" \
+        % (SCREEN_NAME, pp(avg))
 
 # psyco can only compile functions, so wrap code in a function
 
