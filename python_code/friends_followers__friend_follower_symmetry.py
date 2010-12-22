f373592b4dc69ae0b80ba80975be8e5a288756e6
Matthew A. Russell <matthew@zaffra.com>
2010-12-22 13:43:44 -0600

    Fixed a minor bug in reporting output, Mostly style tweaks

--- python_code/friends_followers__friend_follower_symmetry.py
+++ python_code/friends_followers__friend_follower_symmetry.py
@@ -8,27 +8,27 @@ import twitter
 from twitter.oauth_dance import oauth_dance
 import redis
 
-SCREEN_NAME = sys.argv[1]
-
 MAXINT = sys.maxint
-
 locale.setlocale(locale.LC_ALL, '')  # for nice number formatting
 
-# Go to http://twitter.com/apps/new to create an app and get these items
+SCREEN_NAME = sys.argv[1]
 
-consumer_key = ''
-consumer_secret = ''
+# Go to http://twitter.com/apps/new to create an app 
+# to get the consumer key/secret
 
-(oauth_token, oauth_token_secret) = oauth_dance("Your application's name",
-        consumer_key, consumer_secret)
+CONSUMER_KEY = sys.argv[2]
+CONSUMER_SECRET = sys.argv[3]
+
+(oauth_token, oauth_token_secret) = oauth_dance('MiningTheSocialWeb',
+        CONSUMER_KEY, CONSUMER_SECRET)
 t = twitter.Twitter(domain='api.twitter.com', api_version='1',
                     auth=twitter.oauth.OAuth(oauth_token, oauth_token_secret,
-                    consumer_key, consumer_secret))
+                    CONSUMER_KEY, CONSUMER_SECRET))
+
 r = redis.Redis()  # connect using default settings for localhost
 
 # A template-like function that can get friends or followers depending on
 # the function passed into it via func.
-
 
 def _getFriendsOrFollowersUsingFunc(
     func,
@@ -42,7 +42,7 @@ def _getFriendsOrFollowersUsingFunc(
 
     while cursor != 0:
         if wait_period > 3600:  # 1 hour
-            print 'Too many retries. Exiting'
+            print >> sys.stderr, 'Too many retries. Exiting'
             exit()
 
         try:
@@ -52,10 +52,11 @@ def _getFriendsOrFollowersUsingFunc(
             wait_period = 2
         except twitter.api.TwitterHTTPError, e:
             if e.e.code == 401:
-                print 'Encountered 401 Error (Not Authorized)'
+                print >> sys.stderr, 'Encountered 401 Error (Not Authorized)'
                 print 'User %s is protecting their tweets' % (screen_name, )
             elif e.e.code in (502, 503):
-                print 'Encountered %i Error. Trying again in %i seconds' \
+                print >> sys.stderr, \
+                    'Encountered %i Error. Trying again in %i seconds' \
                     % (e.e.code, wait_period)
                 time.sleep(wait_period)
                 wait_period *= 1.5
@@ -65,23 +66,21 @@ def _getFriendsOrFollowersUsingFunc(
                 now = time.time()  # UTC
                 when_rate_limit_resets = status['reset_time_in_seconds']  # UTC
                 sleep_time = when_rate_limit_resets - now
-                print 'Rate limit reached: sleeping for %i secs (%i mins)' \
+                print >> sys.stderr, \
+                    'Rate limit reached: sleeping for %i secs (%i mins)' \
                     % (sleep_time, sleep_time / 60)
                 time.sleep(sleep_time)
                 continue
             else:
                 raise e # Best to handle this based on the specific issue
 
-
         cursor = response['next_cursor']
         scard = r.scard(getRedisIdByScreenName(screen_name, key_name))
-        print 'Fetched %s ids for %s' % (scard, screen_name)
+        print >> sys.stderr, 'Fetched %s ids for %s' % (scard, screen_name)
         if scard >= limit:
             break
 
-
 # A convenience function
-
 
 def getRedisIdByScreenName(screen_name, key_name):
     return 'screen_name$' + screen_name + '$' + key_name
@@ -94,46 +93,50 @@ getFriends = functools.partial(_getFriendsOrFollowersUsingFunc, t.friends.ids,
 getFollowers = functools.partial(_getFriendsOrFollowersUsingFunc,
                                  t.followers.ids, 'follower_ids')
 
-screen_name = SCREEN_NAME
+# Get the data
 
-# get the data
+print >> sys.stderr, 'Getting friends for %s...' % (SCREEN_NAME, )
+getFriends(SCREEN_NAME, limit=MAXINT)
 
-print 'Getting friends for %s...' % (screen_name, )
-getFriends(screen_name, limit=MAXINT)
-print 'Getting followers for %s...' % (screen_name, )
-getFollowers(screen_name, limit=MAXINT)
+print >> sys.stderr, 'Getting followers for %s...' % (SCREEN_NAME, )
+getFollowers(SCREEN_NAME, limit=MAXINT)
 
-# use redis to compute the numbers
+# Use Redis to compute the numbers
 
-n_friends = r.scard(getRedisIdByScreenName(screen_name, 'friend_ids'))
-n_followers = r.scard(getRedisIdByScreenName(screen_name, 'follower_ids'))
-n_friends_diff_followers = r.sdiffstore('temp',
-                                        [getRedisIdByScreenName(screen_name,
-                                        'friend_ids'),
-                                        getRedisIdByScreenName(screen_name,
-                                        'follower_ids')])
-r.delete('temp')
-n_followers_diff_friends = r.sdiffstore('temp',
-                                        [getRedisIdByScreenName(screen_name,
-                                        'follower_ids'),
-                                        getRedisIdByScreenName(screen_name,
-                                        'friend_ids')])
-r.delete('temp')
-n_friends_inter_followers = r.sinterstore('temp',
-        [getRedisIdByScreenName(screen_name, 'follower_ids'),
-        getRedisIdByScreenName(screen_name, 'friend_ids')])
+n_friends = r.scard(getRedisIdByScreenName(SCREEN_NAME, 'friend_ids'))
+
+n_followers = r.scard(getRedisIdByScreenName(SCREEN_NAME, 'follower_ids'))
+
+n_friends_diff_followers = \
+    r.sdiffstore('temp', 
+                 [getRedisIdByScreenName(SCREEN_NAME, 'friend_ids'),
+                 getRedisIdByScreenName(SCREEN_NAME,'follower_ids')])
 r.delete('temp')
 
-print '-' * 50
-print '%s is following %s' % (screen_name, locale.format('%d', n_friends, True))
-print '%s is being followed by %s' % (screen_name, locale.format('%d',
+n_followers_diff_friends = \
+    r.sdiffstore('temp',
+                 [getRedisIdByScreenName(SCREEN_NAME, 'follower_ids'),
+                 getRedisIdByScreenName(SCREEN_NAME, 'friend_ids')])
+r.delete('temp')
+
+n_friends_inter_followers = \
+    r.sinterstore('temp', 
+                  [getRedisIdByScreenName(SCREEN_NAME, 'follower_ids'),
+                  getRedisIdByScreenName(SCREEN_NAME, 'friend_ids')])
+r.delete('temp')
+
+print '%s is following %s' % (SCREEN_NAME, locale.format('%d', n_friends, True))
+
+print '%s is being followed by %s' % (SCREEN_NAME, locale.format('%d',
                                       n_followers, True))
+
 print '%s of %s are not following %s back' % (locale.format('%d',
-        n_friends_diff_followers, True), locale.format('%d', n_followers, True),
-        screen_name)
+        n_friends_diff_followers, True), locale.format('%d', n_friends, True),
+        SCREEN_NAME)
+
 print '%s of %s are not being followed back by %s' % (locale.format('%d',
         n_followers_diff_friends, True), locale.format('%d', n_followers, True),
-        screen_name)
+        SCREEN_NAME)
+
 print '%s has %s mutual friends (followers who are also being followed)' \
-    % (screen_name, locale.format('%d', n_friends_inter_followers, True))
-print '-' * 50
+    % (SCREEN_NAME, locale.format('%d', n_friends_inter_followers, True))
--- python_code/friends_followers__get_friends.py
+++ python_code/friends_followers__get_friends.py
@@ -28,7 +28,8 @@ cursor = -1
 
 while cursor != 0:
     if wait_period > 3600:  # 1 hour
-        print 'Too many retries. Saving partial data to disk and exiting'
+        print >> sys.stderr, \
+            'Too many retries. Saving partial data to disk and exiting'
         f = file('%s.friend_ids' % str(cursor), 'wb')
         cPickle.dump(ids, f)
         f.close()
@@ -40,11 +41,12 @@ while cursor != 0:
         wait_period = 2
     except twitter.api.TwitterHTTPError, e:
         if e.e.code == 401:
-            print 'Encountered 401 Error (Not Authorized)'
-            print 'User %s is protecting their tweets' % (SCREEN_NAME, )
+            print >> sys.stderr, 'Encountered 401 Error (Not Authorized)'
+            print >> sys.stderr, 'User %s is protecting their tweets' % (SCREEN_NAME, )
         elif e.e.code in (502, 503):
-            print 'Encountered %i Error. Trying again in %i seconds' % (e.e.code,
-                    wait_period)
+            print >> sys.stderr, \
+                'Encountered %i Error. Trying again in %i seconds' % \
+                (e.e.code, wait_period)
             time.sleep(wait_period)
             wait_period *= 1.5
             continue
@@ -53,15 +55,15 @@ while cursor != 0:
             now = time.time()  # UTC
             when_rate_limit_resets = status['reset_time_in_seconds']  # UTC
             sleep_time = when_rate_limit_resets - now
-            print 'Rate limit reached. Trying again in %i seconds' % (sleep_time,
-                    )
+            print >> sys.stderr, \
+                'Rate limit reached. Trying again in %i seconds' % (sleep_time,)
             time.sleep(sleep_time)
             continue
         else:
             raise e # Best to handle this on a case by case basis
 
     cursor = response['next_cursor']
-    print 'Fetched %i ids for %s' % (len(ids), SCREEN_NAME)
+    print >> sys.stderr, 'Fetched %i ids for %s' % (len(ids), SCREEN_NAME)
     if len(ids) >= friends_limit:
         break
 
