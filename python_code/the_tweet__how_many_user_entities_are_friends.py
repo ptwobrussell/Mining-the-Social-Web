3582699b128153a0ad53f4fe0f0fa2ad438cacdc
Matthew A. Russell <matthew@zaffra.com>
2010-12-22 22:01:59 -0600

    refactoring another listing to use new twitter__ modules

--- python_code/the_tweet__how_many_user_entities_are_friends.py
+++ python_code/the_tweet__how_many_user_entities_are_friends.py
@@ -3,20 +3,12 @@
 import json
 import redis
 import couchdb
+import sys
+from twitter__util import getRedisIdByScreenName
+from twitter__util import getRedisIdByUserId
 
 SCREEN_NAME = sys.argv[1]
 THRESHOLD = int(sys.argv[2])
-
-# Abstraction convenience functions
-
-
-def getRedisIdByScreenName(screen_name, key_name):
-    return 'screen_name$' + screen_name + '$' + key_name
-
-
-def getRedisIdByUserId(user_id, key_name):
-    return 'user_id$' + user_id + '$' + key_name
-
 
 # Connect using default settings for localhost
 
@@ -37,6 +29,7 @@ for friend_id in friend_ids:
 
 server = couchdb.Server('http://localhost:5984')
 db = server['tweets-user-timeline-' + SCREEN_NAME]
+
 entities_freqs = sorted([(row.key, row.value) for row in
                         db.view('index/entity_count_by_doc', group=True)],
                         key=lambda x: x[1])
@@ -50,6 +43,7 @@ user_entities = [(ef[0])[1:] for ef in entities_freqs if ef[0][0] == '@'
 
 entities_who_are_friends = \
     set(user_entities).intersection(set(friend_screen_names))
+
 entities_who_are_not_friends = \
     set(user_entities).difference(entities_who_are_friends)
 
