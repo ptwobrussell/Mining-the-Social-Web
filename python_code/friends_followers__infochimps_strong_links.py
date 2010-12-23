ca9952687a2e8c94dfffcc6e089875d36eb97bc8
Matthew A. Russell <matthew@zaffra.com>
2010-12-22 18:26:54 -0600

    tweaks to the infochimps strong links api example

--- python_code/friends_followers__infochimps_strong_links.py
+++ python_code/friends_followers__infochimps_strong_links.py
@@ -5,20 +5,18 @@ import urllib2
 import json
 import redis
 
+from twitter__util import getRedisIdByUserId
+
 SCREEN_NAME = sys.argv[1]
 API_KEY = sys.argv[2]
 API_ENDPOINT = \
     'http://api.infochimps.com/soc/net/tw/strong_links.json?screen_name=%s&apikey=%s'
 
-
-def getRedisIdByUserId(user_id, key_name):
-    return 'user_id$' + str(user_id) + '$' + key_name
-
-
 r = redis.Redis()  # default connection settings on localhost
 
 try:
-    response = urllib2.urlopen(API_ENDPOINT % (SCREEN_NAME, API_KEY))
+    url = API_ENDPOINT % (SCREEN_NAME, API_KEY)
+    response = urllib2.urlopen(url)
 except urllib2.URLError, e:
     print 'Failed to fetch ' + url
     raise e
@@ -30,10 +28,13 @@ strong_links = json.loads(response.read())
 print "%s's Strong Links" % (SCREEN_NAME, )
 print '-' * 30
 for sl in strong_links['strong_links']:
+    if sl is None:
+        continue
 
     try:
         user_info = json.loads(r.get(getRedisIdByUserId(sl[0], 'info.json')))
         print user_info['screen_name'], sl[1]
     except Exception, e:
-        print "ERROR: couldn't resolve screen_name for", sl
+        print >> sys.stderr, "ERROR: couldn't resolve screen_name for", sl
+        print >> sys.stderr, "Maybe you haven't harvested data for this person yet?"
 
