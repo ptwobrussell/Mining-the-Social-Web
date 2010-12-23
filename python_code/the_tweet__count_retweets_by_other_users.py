c5ee0214811eba9f8b8d438a50ec2790fcae7b6d
Matthew A. Russell <matthew@zaffra.com>
2010-12-23 03:35:30 -0600

    major refactor to routine that counts the number of times you're retweeted now that Twitter's api seems to be working and properly populating the retweet_count field in statuses

---
+++ python_code/the_tweet__count_retweets_by_other_users.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import couchdb
+from couchdb.design import ViewDefinition
+from prettytable import PrettyTable
+from twitter__util import pp
+
+DB = sys.argv[1]
+
+try:
+    server = couchdb.Server('http://localhost:5984')
+    db = server[DB]
+except couchdb.http.ResourceNotFound, e:
+    print """CouchDB database '%s' not found. 
+Please check that the database exists and try again.""" % DB
+    sys.exit(1)
+
+# Map entities in tweets to the docs that they appear in
+
+def retweetCountMapper(doc):
+    if doc.get('id') and doc.get('text'):
+        yield (doc['retweet_count'], 1)
+
+def summingReducer(keys, values, rereduce):
+    return sum(values)
+
+view = ViewDefinition('index', 'retweets_by_id', retweetCountMapper, 
+                      reduce_fun=summingReducer, language='python')
+
+view.sync(db)
+
+fields = ['Key', 'Value']
+pt = PrettyTable(fields=fields)
+[pt.set_field_align(f, 'l') for f in fields]
+
+retweet_total, num_tweets = 0, 0
+for (k,v) in sorted([(row.key, row.value) for row in 
+                     db.view('index/retweets_by_id', group=True)
+                     if row.key is not None],
+                 key=lambda x: x[0], reverse=True):
+    pt.add_row([k, v])
+
+    if k == "100+":
+        retweet_total += 100*v
+    else:
+        retweet_total += k*v
+
+    num_tweets += v
+
+pt.printt()
+
+print '\n%s tweets generated %s retweets' % (pp(num_tweets), pp(retweet_total),)
