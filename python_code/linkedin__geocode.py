a4d9ac4a19a37a01e754aa58a515592f2684f874
Matthew A. Russell <matthew@zaffra.com>
2010-12-24 14:39:33 -0600

    * fixed a nasty indentation bug with a nested loop * modified output file to be *.kml so that Google Earth will recognize it

--- python_code/linkedin__geocode.py
+++ python_code/linkedin__geocode.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 import cPickle
 from urllib2 import HTTPError
@@ -8,23 +9,28 @@ from cluster import KMeansClustering, centroid
 
 # A very uninteresting helper function to build up an XML tree
 
-from linkedin__kml_util import createKML
+from linkedin__kml_utility import createKML
 
 K = int(sys.argv[1])
-KML_FILE = sys.argv[2]
 
 # Use your own API key here if you use a geocoding service
 # such as Google or Yahoo!
 
-GEOCODING_API_KEY = sys.argv[3]
+GEOCODING_API_KEY = sys.argv[2]
+
+CONNECTIONS_DATA = sys.argv[3]
+
+OUT = "clusters.kmeans.xml"
 
 # Open up your saved connections with extended profile information
 
-extended_connections = cPickle.load(open('../linkedin_connections.pickle'))
+extended_connections = cPickle.load(open(CONNECTIONS_DATA))
 locations = [ec.location for ec in extended_connections]
 g = geocoders.Yahoo(GEOCODING_API_KEY)
 
 # Some basic transforms may be necessary for geocoding services to function properly
+# Here are a few examples that seem to cause problems for Yahoo. You'll probably need
+# to add your own.
 
 transforms = [('Greater ', ''), (' Area', ''), ('San Francisco Bay',
               'San Francisco')]
@@ -40,28 +46,29 @@ for location in locations:
         coords_freqs[location][1] += 1
         continue
     transformed_location = location
-for transform in transforms:
-    transformed_location = transformed_location.replace(*transform)
-    while True:
-        num_errors = 0
-        try:
 
-            # This call returns a generator
+    for transform in transforms:
+        transformed_location = transformed_location.replace(*transform)
+        while True:
+            num_errors = 0
+            try:
 
-            results = g.geocode(transformed_location, exactly_one=False)
+                # This call returns a generator
+
+                results = g.geocode(transformed_location, exactly_one=False)
+                break
+            except HTTPError, e:
+                num_errors += 1
+                if num_errors >= 3:
+                    sys.exit()
+                print >> sys.stderr, e
+                print >> sys.stderr, 'Encountered an urllib2 error. Trying again...'
+        for result in results:
+
+            # Each result is of the form ("Description", (X,Y))
+
+            coords_freqs[location] = [result[1], 1]
             break
-        except HTTPError, e:
-            num_errors += 1
-            if num_errors >= 3:
-                sys.exit()
-            print e
-            print 'Encountered an urllib2 error. Trying again...'
-    for result in results:
-
-        # Each result is of the form ("Description", (X,Y))
-
-        coords_freqs[location] = [result[1], 1]
-        break
 
 # Here, you could optionally segment locations by continent
 # country so as to avoid potentially finding a mean in the middle of the ocean
@@ -84,12 +91,21 @@ for item in kml_items:
     item['contacts'] = '\n'.join(['%s %s.' % (ec.first_name, ec.last_name[0])
                                  for ec in extended_connections if ec.location
                                  == item['label']])
+
 cl = KMeansClustering([coords for (label, coords_list) in expanded_coords
                       for coords in coords_list])
+
 centroids = [{'label': 'CENTROID', 'coords': '%s,%s' % centroid(c)} for c in
              cl.getclusters(K)]
+
 kml_items.extend(centroids)
 kml = createKML(kml_items)
-f = open(KML_FILE, 'w')
+
+if not os.path.isdir('out'):
+    os.mkdir('out')
+
+f = open("out/" + OUT, 'w')
 f.write(kml)
 f.close()
+
+print >> sys.stderr, 'Data pickled to out/' + OUT 
